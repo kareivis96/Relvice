6563b8e96179e871e071dcb3c916ac2959d27b63
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2022-07-14 15:38:04 +0300

    ended make up 'contact' block

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-let contacts__card = document.querySelector('.contacts__card');
+let contacts__card_1 = document.querySelector('.contacts__card_1');
+let contacts__card_2 = document.querySelector('.contacts__card_2');
+let contacts__card_3 = document.querySelector('.contacts__card_3');
 
-contacts__card.addEventListener('mouseenter', function() {
-  contacts__card.style.background = 'green';
+let contacts__cardImg_1 = document.querySelector('.contacts__card-img_1');
+let contacts__cardImg_2 = document.querySelector('.contacts__card-img_2');
+let contacts__cardImg_3 = document.querySelector('.contacts__card-img_3');
+
+
+contacts__card_1.addEventListener('mouseenter', function() {
+  contacts__cardImg_1.src = '../img/contacts/1-img-white.png';
+});
+contacts__card_1.addEventListener('mouseleave', function () {
+  contacts__cardImg_1.src = '../img/contacts/1-img-green.png';
 });
 
-contacts__card.addEventListener('mouseleave', function () {
-  contacts__card.style.background = 'white';
+
+contacts__card_2.addEventListener('mouseenter', function () {
+  contacts__cardImg_2.src = '../img/contacts/2-img-white.png';
+});
+contacts__card_2.addEventListener('mouseleave', function () {
+  contacts__cardImg_2.src = '../img/contacts/2-img-green.png';
+});
+
+
+contacts__card_3.addEventListener('mouseenter', function () {
+  contacts__cardImg_3.src = '../img/contacts/3-img-white.png';
+});
+contacts__card_3.addEventListener('mouseleave', function () {
+  contacts__cardImg_3.src = '../img/contacts/3-img-green.png';
 });
