be8268545438e2c095cc472e261c169bf073d533
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2022-07-16 20:08:20 +0300

    make up footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// --------------------------- скрипт на смену иконок при наведении в блоке contacts -----------------------
 let contacts__card_1 = document.querySelector('.contacts__card_1');
 let contacts__card_2 = document.querySelector('.contacts__card_2');
 let contacts__card_3 = document.querySelector('.contacts__card_3');
@@ -14,14 +15,12 @@ contacts__card_1.addEventListener('mouseleave', function () {
   contacts__cardImg_1.src = '../img/contacts/1-img-green.png';
 });
 
-
 contacts__card_2.addEventListener('mouseenter', function () {
   contacts__cardImg_2.src = '../img/contacts/2-img-white.png';
 });
 contacts__card_2.addEventListener('mouseleave', function () {
   contacts__cardImg_2.src = '../img/contacts/2-img-green.png';
 });
-
 
 contacts__card_3.addEventListener('mouseenter', function () {
   contacts__cardImg_3.src = '../img/contacts/3-img-white.png';
